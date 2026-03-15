296c5cec224dd545e64ecc366e1ab16e043a3c88
Jevaughn Stewart <stewartjevaughn1@gmail.com>
2026-03-15 03:41:36 -0500

    updates

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,8 +30,8 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/Jascinth Stewart-Resume.pdf"
-                download="Jascinth Stewart-Resume.pdf"
+                href="/Jevaughn Stewart Resume .pdf"
+                download="Jevaughn Stewart Resume .pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download Resume
